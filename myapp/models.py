e507d3a5bd2469d717a1636d6d8f4e6062d5e335
EliasDFranco <eliasfranco20160055@gmail.com>
2025-02-19 16:42:32 -0300

    Modelos BD sqlite, creando clases Prohejt y Task

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):  # Cree está clase Project
+    name = models.CharField(max_length=250)
+    
+class Task(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) #Aquí designo que project va a relacionarse con la clase Project, tmb agregue que cuando se elimine Project, 
+                                                                    #se elimine en cascadsa los elementos que dependan o tengan relacion con este.
+    
+    
+    
