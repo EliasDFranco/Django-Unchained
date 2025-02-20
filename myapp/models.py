b96d361629a609131eeef3c41cca968a5ef4cb75
EliasDFranco <eliasfranco20160055@gmail.com>
2025-02-20 12:01:34 -0300

    Cambiando cosas y manipulando el panel de Admin

--- myapp/models.py
+++ myapp/models.py
@@ -4,9 +4,14 @@ from django.db import models
 class Project(models.Model):  # Cree está clase Project
     name = models.CharField(max_length=250)
     
+    def __str__(self):
+        return self.name # Para que en Django Admin se pueda ver los "Nombres" de los proyectos
+    
 class Task(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE) #Aquí designo que project va a relacionarse con la clase Project, tmb agregue que cuando se elimine Project, 
-                                                                    #se elimine en cascadsa los elementos que dependan o tengan relacion con este.
+
+    def __str__(self):
+        return self.title  + ' - ' + self.project.name                                                        #se elimine en cascadsa los elementos que dependan o tengan relacion con este.
     
