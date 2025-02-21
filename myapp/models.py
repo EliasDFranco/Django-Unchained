764eb7acb1ad23eae3111d751402d929199002ff
EliasDFranco <eliasfranco20160055@gmail.com>
2025-02-21 11:47:21 -0300

    Jinja Conditionals |  Tarea pendiente o realizado

--- myapp/models.py
+++ myapp/models.py
@@ -11,7 +11,9 @@ class Task(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE) #Aquí designo que project va a relacionarse con la clase Project, tmb agregue que cuando se elimine Project, 
-
+                                                                    # se elimine en cascadsa los elementos que dependan o tengan relacion con este.
+    done = models.BooleanField(default=False)
+    
     def __str__(self):
-        return self.title  + ' - ' + self.project.name                                                        #se elimine en cascadsa los elementos que dependan o tengan relacion con este.
+        return self.title  + ' - ' + self.project.name                                                       
     
